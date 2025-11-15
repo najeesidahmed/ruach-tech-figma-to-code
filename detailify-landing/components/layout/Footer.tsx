1eb89693353f4a2fcc2759d56b2582996da1b088
najee sidahmed <najeekamil@msn.com>
2025-11-15 11:11:24 +0000

    create footer links object array and export function

--- detailify-landing/components/layout/Footer.tsx
+++ detailify-landing/components/layout/Footer.tsx
@@ -1,7 +1,20 @@
+const footerLinks = [
+    {
+        title: "Product",
+        links: ["Features", "Pricing", "Integrations", "Changelog"]
+    },
+    {
+        title: "Company",
+        links: ["About", "Blog", "Careers", "Contact"]
+    },
+    {
+        title: "Legal",
+        links: ["Privacy", "Terms", "Security"]
+    },
+];
+
 export function Footer() {
     return (
-        <footer>
-            <p>Footer</p>
-        </footer>
+        <footer>Footer</footer>
     )
 }
