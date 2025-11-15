8165a425416eee210f613a9c269e29792d5e3876
najee sidahmed <najeekamil@msn.com>
2025-11-15 12:22:01 +0000

    add global colour variables and tailwind theme colours

--- detailify-landing/tailwind.config.js
+++ detailify-landing/tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: {
+          white: "var(--text-white)",
+          footer_light: "var(--text-footer-ligh)",
+          text_banner: "var(--text-cta-banner)",
+          p: "var(--text-paragraph)",
+          card_p: "var(--text-card-p)",
+          card_title: "var(--text-card-heading)",
+        },
+        brand: {
+          blue: "var(--brand-blue)",
+          purple: "var(--brand-purple)"
+        },
+        button: {
+          dark: "var(--button-dark)"
+        }
+      },
+    },
   },
   plugins: [],
-}
+};
